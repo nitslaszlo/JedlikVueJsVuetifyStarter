438de8d44ef60db34a063206841a8cbf80ce5e1d
nitslaszlo <nitslaszlo@gmail.com>
2018-09-10 17:55:11 +0200

    up

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,14 @@
               outline
             ></v-text-field>
           </v-flex>
-  
+
+          <v-flex xs12 sm6 md3>
+            <v-btn color="success">Success</v-btn>
+            <v-btn color="error">Error</v-btn>
+            <v-btn color="warning">Warning</v-btn>
+            <v-btn color="info">Info</v-btn>
+          </v-flex>
+
         </v-layout>
       </v-container>
     </v-form>
