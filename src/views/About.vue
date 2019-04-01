313b46ecab7b34813a60ed8749d02f4089e76761
nitslaszlo <nitslaszlo@gmail.com>
2019-04-01 09:30:15 +0200

    up

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,47 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div id="app">
+      <v-app id="inspire">
+        <v-container fluid grid-list-md>
+          <v-layout row wrap>
+            <v-flex d-flex xs12 sm6 md4>
+              <v-card color="purple" dark>
+                <v-card-title primary class="title">Lorem</v-card-title>
+                <v-card-text>{{ lorem }}</v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex d-flex xs12 sm6 md3>
+              <v-layout row wrap>
+                <v-flex d-flex>
+                  <v-card color="indigo" dark>
+                    <v-card-text>{{ lorem.slice(0, 70) }}</v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex d-flex>
+                  <v-layout row wrap>
+                    <v-flex v-for="n in 2" :key="n" d-flex xs12>
+                      <v-card color="red lighten-2" dark>
+                        <v-card-text>{{ lorem.slice(0, 40) }}</v-card-text>
+                      </v-card>
+                    </v-flex>
+                  </v-layout>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex d-flex xs12 sm6 md2 child-flex>
+              <v-card color="green lighten-2" dark>
+                <v-card-text>{{ lorem.slice(0, 90) }}</v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex d-flex xs12 sm6 md3>
+              <v-card color="blue lighten-2" dark>
+                <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-app>
+    </div>
   </div>
 </template>
 
@@ -8,7 +49,10 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class About extends Vue {}
+export default class About extends Vue {
+  // tslint:disable-next-line:max-line-length
+  private lorem: string = "Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.";
+}
 </script>
 
 <style>
